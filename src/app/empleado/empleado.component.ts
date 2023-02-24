470ee3bdf93c2d73759a139b4478d96f7a3ce16a
Fausto Flores <tito_flores21@hotmail.com>
2023-02-24 17:11:15 -0600

    update component empleado

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -7,12 +7,47 @@ import { Component } from '@angular/core';
 })
 export class EmpleadoComponent {
 
+
   private nombre='Carlos';
   private apellido='Zepeda';
   private edad=27;
   private empresa='Bar-beria';
 
+  habilitacionCuadro =false;
+
+  userRegistrado =false;
+
+  textoDeRegistro ="No hay nadie Registrado";
+
+  getRegistroUser(){
+
+    this.userRegistrado = false;
+    // return this.userRegistrado;
+
+  }
+
+  setUsuarioregistrado(event:Event){
+    // alert('el usuario se acaba de registrar');
+
+    // alert(event.target);
+    // this.textoDeRegistro=`El usuario ${(<HTMLInputElement>event.target).value} esta registrado`;
+
+    if ((<HTMLInputElement>event.target).value=="si") {
+      this.textoDeRegistro="El usuario se acaba de registrar";
+    } else {
+      this.textoDeRegistro="No hay nadie registrado";
+    }
+    
+  }
+
+  // setUsuarioNoregistrado(event:Event){
+
+  //   this.textoDeRegistro="El usuario no esta registrado";
+  // }
+
   // llamaEmpresa(value:string){}
+
+
 
   getnombre(){return this.nombre;}
 
